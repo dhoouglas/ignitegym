e063b91bef3d22b32d43272e06dc5967bfb47d4e
dhoouglas <dhoouglas@hotmail.com>
2025-01-08 13:26:55 -0300

    fix: input (data) validation with the yup library

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -7,6 +7,8 @@ import {
   ScrollView,
 } from "@gluestack-ui/themed";
 import { useForm, Controller } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
 
 import backgroundImg from "@assets/background.png";
 import Logo from "@assets/logo.svg";
@@ -22,12 +24,28 @@ type FormDataProps = {
   password_confirm: string;
 };
 
+const signUpSchema = yup.object({
+  name: yup.string().required("Informe o nome."),
+  email: yup.string().required("Informe o e-mail.").email("E-mail inválido."),
+  password: yup
+    .string()
+    .required("Informe a senha.")
+    .min(6, "A senha deve ter pelo menos 6 dígitos."),
+  password_confirm: yup
+    .string()
+    .required("Confirme a senha.")
+    .oneOf([yup.ref("password"), ""], "A confirmação da senha não confere."),
+});
+
+// type FormData = yup.InferType<typeof signUpSchema>;
 export function SignUp() {
   const {
     control,
     handleSubmit,
     formState: { errors },
-  } = useForm<FormDataProps>();
+  } = useForm<FormDataProps>({
+    resolver: yupResolver(signUpSchema),
+  });
 
   const navigation = useNavigation<AuthNavigatorRoutesProps>();
 
@@ -74,7 +92,6 @@ export function SignUp() {
             <Controller
               control={control}
               name="name"
-              rules={{ required: "Informe o nome" }}
               render={({ field: { onChange, value } }) => (
                 <Input
                   placeholder="Nome"
@@ -88,13 +105,6 @@ export function SignUp() {
             <Controller
               control={control}
               name="email"
-              rules={{
-                required: "Informe o email",
-                pattern: {
-                  value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-                  message: "E-mail inválido",
-                },
-              }}
               render={({ field: { onChange, value } }) => (
                 <Input
                   placeholder="Email"
@@ -116,9 +126,11 @@ export function SignUp() {
                   secureTextEntry
                   onChangeText={onChange}
                   value={value}
+                  errorMessage={errors.password?.message}
                 />
               )}
             />
+
             <Controller
               control={control}
               name="password_confirm"
@@ -130,6 +142,7 @@ export function SignUp() {
                   value={value}
                   onSubmitEditing={handleSubmit(handleSignUp)}
                   returnKeyType="send"
+                  errorMessage={errors.password_confirm?.message}
                 />
               )}
             />
