06db6450dc55e89bafbb6fed8560608d3b86c4c7
dhoouglas <dhoouglas@hotmail.com>
2025-01-10 12:48:48 -0300

    feat: saving user data in async storage when reloading the application

--- mobile/src/routes/index.tsx
+++ mobile/src/routes/index.tsx
@@ -7,12 +7,17 @@ import { AppRoutes } from "./app.routes";
 import { gluestackUIConfig } from "../../config/gluestack-ui.config";
 
 import { useAuth } from "@hooks/useAuth";
+import { Loading } from "@components/Loading";
 
 export function Routes() {
-  const { user } = useAuth();
+  const { user, isLoadingUserStorageData } = useAuth();
 
   const theme = DefaultTheme;
   theme.colors.background = gluestackUIConfig.tokens.colors.gray700;
+
+  if (isLoadingUserStorageData) {
+    return <Loading />;
+  }
 
   return (
     <Box flex={1} bg="$gray700">
